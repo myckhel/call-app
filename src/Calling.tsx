24837d5aabd1915ccdaa13690dbffad87b6d66a9
Myckhel <myckhel123@gmail.com>
2024-04-13 12:06:30 +0100

    moved init call to call manager

--- src/Calling.tsx
+++ src/Calling.tsx
@@ -1,6 +1,5 @@
 import { useRootMemoSelector } from "use-redux-states";
 import { User } from "./Call";
-import { useEffect, useRef } from "react";
 import CallManager from "./utils/CallManager";
 
 export default function Calling() {
@@ -10,41 +9,34 @@ export default function Calling() {
   );
 
   const onEndCall = () => CallManager.endCall();
-  const onRef = (video) => CallManager.createMedia({ video });
 
-  useEffect(() => {
-    // Access the user's webcam and display it in the video element
-    navigator.mediaDevices
-      .getUserMedia({ video: true, audio: true })
-      .then((stream) => CallManager.addStreamToMedia("video", stream))
-      .catch((error) => {
-        console.error("Error accessing webcam:", error);
-      });
-
-    // Cleanup function to stop the video stream when the component unmounts
-    return () => {
-      CallManager.stopMediaTracks();
-    };
-  }, []);
+  const onRef = (video) => {
+    if (video) {
+      CallManager.createMedia({ video });
+      CallManager.initMedia();
+    }
+  };
 
   return (
     <div className="relative">
       <div>
         Calling {otherUser?.name}({otherUser.email}) with id: {otherUserId}
       </div>
-      <video
-        ref={onRef}
-        className="w-full max-w-sm max-h-full"
-        autoPlay
-        playsInline
-        muted // Mute the video to prevent feedback
-      ></video>
-      <button
-        onClick={onEndCall}
-        className="absolute top-4 right-4 bg-red-500 text-white py-2 px-4 rounded-md hover:bg-red-600 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-red-500"
-      >
-        End Call
-      </button>
+      <div>
+        <video
+          ref={onRef}
+          className="w-full max-w-sm max-h-full"
+          autoPlay
+          playsInline
+          muted
+        />
+        <button
+          onClick={onEndCall}
+          className="absolute top-4 right-4 bg-red-500 text-white py-2 px-4 rounded-md hover:bg-red-600 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-red-500"
+        >
+          End Call
+        </button>
+      </div>
     </div>
   );
 }
--- src/utils/CallManager.ts
+++ src/utils/CallManager.ts
@@ -9,21 +9,24 @@ interface CallMedia {
 
 export type MediaType = "video" | "audio";
 
+interface Streams {
+  localStream?: MediaStream;
+  remoteStream?: MediaStream;
+}
+
 export default class CallManager {
   private static store = store;
   private static media?: CallMedia;
-
-  static cleanup = () => this.endCall();
-
-  static stopMediaTracks = () => {
-    const source = this.media?.audio?.srcObject || this.media?.video?.srcObject;
-    if (source) {
-      // @ts-expect-error unkown
-      const tracks = source.getTracks();
-      tracks.forEach((track) => track.stop());
-      this.media = undefined;
-    }
+  private static peerConnection?: RTCPeerConnection;
+  private static callTypes = ["video", "audio"];
+  private static streams: Streams = {
+    localStream: undefined,
+    remoteStream: undefined,
   };
+
+  static get isCalling() {
+    return this.getOtherUserId();
+  }
 
   static createMedia = (media: CallMedia) => (this.media = media);
 
@@ -31,20 +34,77 @@ export default class CallManager {
     if (this.media && this.media[type]) {
       // @ts-expect-error No idea
       this.media[type].srcObject = stream;
+      // @ts-expect-error No idea
+      this.media[type].volume = 0;
     }
   };
 
-  static startCall = (id) => this.store.dispatch(setOtherUserId(id));
+  static startCall = async (id) => this.store.dispatch(setOtherUserId(id));
+
+  static initMedia = async () => {
+    const stream = await this.getLocalStream();
+
+    this.addStreamToMedia("video", stream);
+  };
+
+  static getLocalStream = async () => {
+    if (this.streams?.localStream) return this.streams.localStream;
+
+    const stream = await this.getMediaStream();
+    this.streams.localStream = stream;
+
+    return stream;
+  };
+
+  static getMediaStream = () =>
+    navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: true,
+    });
 
   static endCall = () => {
-    const otherUserId = (this.store?.getState() as RootState)?.call
-      ?.otherUserId;
+    const otherUserId = this.getOtherUserId();
 
     if (otherUserId) {
       this.stopMediaTracks();
+      this.clearLocalStream();
+
       this.store.dispatch(setOtherUserId(undefined));
     } else {
       console.error("No active call");
+    }
+  };
+
+  static getOtherUserId = () =>
+    (this.store?.getState() as RootState)?.call?.otherUserId;
+
+  static clearLocalStream = () => {
+    this.streams.localStream?.getTracks().forEach((track) => track.stop());
+    this.streams.localStream = undefined;
+  };
+
+  static clearMediaSource = () => {
+    this.callTypes.map((type) => {
+      if (this.media && this.media[type]) {
+        this.media[type].srcObject = undefined;
+      }
+    });
+    this.media = undefined;
+  };
+
+  static cleanup = () => this.endCall();
+
+  static getMediaSource = () =>
+    this.media?.audio?.srcObject || this.media?.video?.srcObject;
+
+  static stopMediaTracks = () => {
+    const source = this.getMediaSource();
+
+    if (source) {
+      // @ts-expect-error unkown
+      const tracks = source.getTracks();
+      tracks.forEach((track) => track.stop());
+      this.clearMediaSource();
     }
   };
 }
